ee0cf05bae324b52fc7d087bada1876b76c1f40f
aAlejandroNM <adrian.neira@uptc.edu.co>
2024-06-09 21:07:08 -0500

    Feat package structure

---
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/SpringBootClienteappApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.app.spring_boot_clienteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClienteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClienteappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cliente/app/spring_boot_clienteapp/SpringBootClienteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.app.spring_boot_clienteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootClienteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
