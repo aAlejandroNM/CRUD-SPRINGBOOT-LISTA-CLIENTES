1c604e4cef20d7908c08106946f1c8921a8c95c4
aAlejandroNM <adrian.neira@uptc.edu.co>
2024-07-14 17:52:55 -0500

    feat sparkles : Mostrar Clientes con tablas de bootstrap

---
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/controller/ClienteController.java
@@ -0,0 +1,27 @@
+package com.cliente.app.spring_boot_clienteapp.controller;
+
+import com.cliente.app.spring_boot_clienteapp.models.entity.Cliente;
+import com.cliente.app.spring_boot_clienteapp.models.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/views/clientes")
+public class ClienteController {
+
+    @Autowired
+    private IClienteService clienteService;
+    @GetMapping("/")
+    public String listarClientes(Model model){
+
+        List<Cliente> listadoClientes = clienteService.listarTodos();
+        model.addAttribute("titulo","Lista de Clientes");
+        model.addAttribute("clientes",listadoClientes);
+        return "/views/clientes/listar";
+    }
+}
--- src/main/java/com/cliente/app/spring_boot_clienteapp/models/service/ClienteServiceImpl.java
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/models/service/ClienteServiceImpl.java
@@ -2,13 +2,14 @@ package com.cliente.app.spring_boot_clienteapp.models.service;
 
 import com.cliente.app.spring_boot_clienteapp.models.entity.Cliente;
 import com.cliente.app.spring_boot_clienteapp.models.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ClienteServiceImpl implements IClienteService {
-
+    @Autowired
     private ClienteRepository clienteRepository;
     @Override
     public List<Cliente> listarTodos() {
