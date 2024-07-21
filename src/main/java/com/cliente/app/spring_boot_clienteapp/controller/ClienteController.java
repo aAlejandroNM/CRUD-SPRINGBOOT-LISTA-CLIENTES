b0518b22b41bf4c291842b8d95354a95d7958425
aAlejandroNM <adrian.neira@uptc.edu.co>
2024-07-21 16:08:25 -0500

    feat: Funcionalidad Boton Guardar para enviar datos a la DB

--- src/main/java/com/cliente/app/spring_boot_clienteapp/controller/ClienteController.java
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/controller/ClienteController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -38,6 +40,14 @@ public class ClienteController {
         model.addAttribute("cliente", cliente);
         model.addAttribute("ciudades", listCiudades);
         return "/views/clientes/frmCrear";
+    }
+
+    @PostMapping("/save")
+    public String guardar(@ModelAttribute Cliente cliente) {
+
+        clienteService.guardar(cliente);
+        System.out.println("Cliente guardado con exito!!!");
+        return "redirect:/views/clientes/";
     }
 
 }
