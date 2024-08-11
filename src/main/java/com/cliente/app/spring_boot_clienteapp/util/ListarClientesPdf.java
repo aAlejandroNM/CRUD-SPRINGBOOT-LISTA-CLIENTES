bd956ebe272357bd471a68eef13d31759f2f20b9
aAlejandroNM <adrian.neira@uptc.edu.co>
2024-08-10 19:26:16 -0500

    feat: Cambio presentacion del pdf de lista de clientes

--- src/main/java/com/cliente/app/spring_boot_clienteapp/util/ListarClientesPdf.java
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/util/ListarClientesPdf.java
@@ -1,7 +1,9 @@
 package com.cliente.app.spring_boot_clienteapp.util;
 
 import com.cliente.app.spring_boot_clienteapp.models.entity.Cliente;
-import com.lowagie.text.Document;
+import com.lowagie.text.*;
+import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 
@@ -20,6 +23,24 @@ public class ListarClientesPdf extends AbstractPdfView {
         @SuppressWarnings("unchecked")
         List<Cliente> listadoClientes = (List<Cliente>) model.get("clientes");
 
+        document.setPageSize(PageSize.LETTER.rotate());
+        document.setMargins(-20,-20,40,20);
+        document.open();
+
+        PdfPTable tablaTitulo = new PdfPTable(1);
+        PdfPCell celda = null;
+
+        Font fuenteTitulo = FontFactory.getFont("Helvetica",16,Color.BLACK);
+        celda = new PdfPCell(new Phrase("LISTADO GENERAL DE CLIENTES",fuenteTitulo));
+        celda.setBorder(0);
+        celda.setBackgroundColor(new Color(62,73,232));
+        celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+        celda.setVerticalAlignment(Element.ALIGN_CENTER);
+        celda.setPadding(20);
+
+        tablaTitulo.addCell(celda);
+        tablaTitulo.setSpacingAfter(25);
+
         PdfPTable tablaClientes = new PdfPTable(6);
 
         listadoClientes.forEach(cliente -> {
@@ -30,6 +51,7 @@ public class ListarClientesPdf extends AbstractPdfView {
             tablaClientes.addCell(cliente.getEmail());
             tablaClientes.addCell(cliente.getCiudad().getCiudad());
         });
+        document.add(tablaTitulo);
         document.add(tablaClientes);
     }
 }
