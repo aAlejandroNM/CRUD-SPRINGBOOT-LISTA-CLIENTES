d7ba6850bf39649993a5187ac9fbe66ad301bae5
aAlejandroNM <adrian.neira@uptc.edu.co>
2024-08-23 18:14:15 -0500

    feat: Configuracion Autorizaciones Http

--- src/main/java/com/cliente/app/spring_boot_clienteapp/WebSecurityConfig.java
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
@@ -15,6 +16,18 @@ public class WebSecurityConfig{
 
     @Autowired
     private BCryptPasswordEncoder passEncoder;
+
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().requestMatchers("/index","/home","/","/css/**","/js/**","/images/**")
+                .permitAll().requestMatchers("/views/clientes/").hasAnyRole("USER")
+                .requestMatchers("/views/clientes/create").hasAnyRole("ADMIN")
+                .requestMatchers("/views/clientes/save").hasAnyRole("ADMIN")
+                .requestMatchers("/views/clientes/edit/**").hasAnyRole("ADMIN")
+                .requestMatchers("/views/clientes/delete/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated();
+
+    }
 
     @Autowired
     public void configurerSecurityGlobal(AuthenticationManagerBuilder builder) throws Exception {
