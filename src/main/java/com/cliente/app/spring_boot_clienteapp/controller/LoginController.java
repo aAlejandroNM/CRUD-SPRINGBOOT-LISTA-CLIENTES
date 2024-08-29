bf483d4e8502970e22b212ddd990d028f017e0e7
aAlejandroNM <adrian.neira@uptc.edu.co>
2024-08-29 15:25:17 -0500

    feat: Configuracion de mensajes de inicio de sesion y cierre de sesion

--- src/main/java/com/cliente/app/spring_boot_clienteapp/controller/LoginController.java
+++ src/main/java/com/cliente/app/spring_boot_clienteapp/controller/LoginController.java
@@ -1,12 +1,34 @@
 package com.cliente.app.spring_boot_clienteapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
+    public String login(@RequestParam(value ="error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model, Principal principal, RedirectAttributes attributes){
+
+        if (error != null){
+            model.addAttribute("error", "ERROR: Usuario y/o contraseña invalidos");
+        }
+
+        if (principal != null){
+            attributes.addFlashAttribute("warning","ATENCION: Ya inicio sesion previamente");
+            return "redirect:/home";
+        }
+        if (logout != null){
+            model.addAttribute("success", "ATENCION: Cierre de sesion exitoso !!");
+        }
+
+
+
         return "login";
     }
 }
